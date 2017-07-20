84fcea952fe45bdd94c8d67b3ce711774c4defdd
coolsnowmail <cool.snow@mail.ru>
2017-07-20 17:26:27 +0300

    user can manage with task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,51 @@
 class TasksController < ApplicationController
   skip_before_action :authorize_user, only: [:index]
+  before_action :set_task, only: [:edit, :update, :destroy]
   def new
+    @task = Task.new
   end
 
   def create
+    @task = @current_user.tasks.build(task_params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to users_show_user_path(@current_user) }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to users_show_user_path(@current_user) }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def edit
   end
 
   def destroy
-  end
+    @task.destroy
+    return render partial: 'destroy'
 
-  def show
   end
 
   def index
     @tasks = Task.all
   end
+
+    private
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:description, :name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :tasks
+  resources :tasks, only: [:index, :new, :edit, :create, :update, :destroy]
 
   # Example resource route with options:
   #   resources :products do
